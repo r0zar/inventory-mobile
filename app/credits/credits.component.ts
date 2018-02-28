3d58f5b679b794bd68e9f8959c41195847a50eb6
Ross Ragsdale <ross.ragsdale@gmail.com>
2018-02-27 22:15:12 -0600

    Polish credits and payment pages.

--- app/credits/credits.component.ts
+++ app/credits/credits.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { PageRoute, RouterExtensions } from "nativescript-angular/router";
+import * as elementRegistryModule from 'nativescript-angular/element-registry';
+elementRegistryModule.registerElement("CardView", () => require("nativescript-cardview").CardView);
 
 @Component({
     selector: "Credits",
